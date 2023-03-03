26ce84146326d2d52e8852f443a5dd71ee21269e
João Henrique <joaohnsousa@gmail.com>
2023-03-03 00:09:15 -0300

    03/03
    Aprendendo sobre o StateLift

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Hello from './components/Hello';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
+
+import Saudacao from './components/Saudacao';
 import List from './components/List';
 import Eventos from './components/Eventos';
 import Form from './components/Form';
 import Condicional from './components/Condicional';
 import Lista from './components/Lista/Lista';
-
+import { useState } from 'react';
+import SeuNome from './components/SeuNome';
 
 function App() {
-  const nome = 'Luiz'
-  const meusItens = ['React', 'Vue', 'Angular', 'Node.js']
+  const meusItens = ['React, Vue, Angular, Node']
+  const [nome, setNome] = useState()
+
   return (
     <div className="App">
-      {/* <Hello/>
-      <SayMyName nome={nome}/>
-      <Pessoa nome={nome} idade='16' profissao="Programador"/>
-      <List/> */}
       <h1>Testando Eventos</h1>
       <Eventos/>
       <h1>Testando Form</h1>
@@ -27,6 +24,9 @@ function App() {
       <Condicional/>
       <h1>Renderização de Lista:</h1>
       <Lista itens={meusItens}></Lista>
+      <h1> Entendendo StateLift:</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome}/>
     </div>
   );
 }
